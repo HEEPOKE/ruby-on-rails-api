3ec7cf29ae668dae80c2046233198cddc4d13bed
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-05 15:35:50 +0700

    :money_with_wings: update table users

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_082857) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.string "tel"
     t.string "role", default: "USER"
     t.datetime "created_at", null: false
